d6f14bebde7414ea962f76b389b1c90a3c3458bb
Lane Bretschneider <lanebret85@gmail.com>
2023-10-16 21:18:29 -0700

    test: add test for items index request

--- spec/requests/api/v1/items_request_spec.rb
+++ spec/requests/api/v1/items_request_spec.rb
@@ -2,6 +2,25 @@ require "rails_helper"
 
 describe "Items API" do
   it "sends a list of all items" do
-     
+    create_list(:item, 10)
+
+    get '/api/v1/items'
+
+    expect(response).to be_successful
+
+    items = JSON.parse(response.body,symbolize_names: true)
+
+    expect(items.count).to eq(10)
+
+    items.each do |item|
+      expect(item).to have_key(:id)
+      expect(item[:id]).to be_an(Integer)
+
+      expect(item).to have_key(:description)
+      expect(item[:description]).to be_a(String)
+
+      expect(item).to have_key(:unit_price)
+      expect(item[:unit_price]).to be_a(Float)
+    end
   end
 end
