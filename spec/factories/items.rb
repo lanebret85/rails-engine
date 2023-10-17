9bbd3ec4652befeffeab3569598944c75673ed7e
Lane Bretschneider <lanebret85@gmail.com>
2023-10-16 22:11:33 -0700

    feat: add merchant factory association

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -3,5 +3,6 @@ FactoryBot.define do
     name { "Item #{Faker::Lorem.word(exclude_words: ['id', 'error'])} #{Faker::Lorem.word(exclude_words: ['id', 'error'])}" }
     description { Faker::Lorem.paragraph(sentence_count: 3, supplemental: false, random_sentences_to_add: 2) }
     unit_price { Faker::Number.decimal(l_digits: 3, r_digits: 2).to_s }
+    merchant
   end
 end
