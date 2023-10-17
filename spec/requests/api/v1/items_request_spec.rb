d98790c54e3e7312760e255ad854ff6302f58bb7
Lane Bretschneider <lanebret85@gmail.com>
2023-10-17 15:52:44 -0700

    test: add tests for items show endpoint, beef up test for items index endpoint by testing for every layer of the response body

--- spec/requests/api/v1/items_request_spec.rb
+++ spec/requests/api/v1/items_request_spec.rb
@@ -8,7 +8,11 @@ describe "Items API" do
 
     expect(response).to be_successful
 
-    items = JSON.parse(response.body,symbolize_names: true)[:data]
+    items_hash = JSON.parse(response.body,symbolize_names: true)
+
+    expect(items_hash).to be_a(Hash)
+    
+    items = items_hash[:data]
 
     expect(items.count).to eq(10)
 
@@ -20,6 +24,7 @@ describe "Items API" do
       expect(item[:type]).to eq("item")
 
       expect(item).to have_key(:attributes)
+      expect(item[:attributes]).to be_a(Hash)
 
       item_attributes = item[:attributes]
 
@@ -29,5 +34,39 @@ describe "Items API" do
       expect(item_attributes).to have_key(:unit_price)
       expect(item_attributes[:unit_price]).to be_a(Float)
     end
+  end
+
+  it "sends a single item result" do
+    id = create(:item).id
+
+    get "/api/v1/items/#{id}"
+
+    expect(response).to be_successful
+
+    item_hash = JSON.parse(response.body,symbolize_names: true)
+
+    expect(item_hash).to be_a(Hash)
+    
+    item = item_hash[:data]
+
+    expect(item).to have_key(:id)
+    expect(item[:id]).to be_a(String)
+
+    expect(item).to have_key(:type)
+    expect(item[:type]).to eq("item")
+
+    expect(item).to have_key(:attributes)
+    expect(item[:attributes]).to be_a(Hash)
+
+    item_attributes = item[:attributes]
+
+    expect(item_attributes).to have_key(:name)
+    expect(item_attributes[:name]).to be_a(String)
+
+    expect(item_attributes).to have_key(:description)
+    expect(item_attributes[:description]).to be_a(String)
+
+    expect(item_attributes).to have_key(:unit_price)
+    expect(item_attributes[:unit_price]).to be_a(Float)
   end
 end
