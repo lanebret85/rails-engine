b6a6f3f8f1070446232c84b3b55fa10d551fb319
Lane Bretschneider <lanebret85@gmail.com>
2023-10-17 16:31:09 -0700

    feat: add create action with render json, add strong params method

--- app/controllers/api/v1/items_controller.rb
+++ app/controllers/api/v1/items_controller.rb
@@ -8,6 +8,16 @@ module Api
       def show
         render json: ItemSerializer.new(Item.find(params[:id]))
       end
+
+      def create
+        render json: Item.create!(item_params)
+      end
+
+      private
+
+        def item_params
+          params.permit(:name, :description, :unit_price, :merchant_id)
+        end
     end
   end
 end
