6101253aecc03b6a032d9e9acce79649e12d133a
Lane Bretschneider <lanebret85@gmail.com>
2023-10-17 17:37:01 -0700

    test: add test for items destroy endpoint

--- spec/requests/api/v1/items_request_spec.rb
+++ spec/requests/api/v1/items_request_spec.rb
@@ -118,4 +118,16 @@ describe "Items API" do
     expect(item.unit_price).to eq(current_unit_price)
     expect(item.merchant_id).to eq(current_merchant_id)
   end
+
+  it "can destroy an item" do
+    item = create(:item)
+
+    expect(Item.count).to eq(1)
+
+    delete "/api/v1/items/#{item.id}"
+
+    expect(response).to be_successful
+    expect(Item.count).to eq(0)
+    expect{ Item.find(item.id) }.to raise_error(ActiveRecord::RecordNotFound)
+  end
 end
