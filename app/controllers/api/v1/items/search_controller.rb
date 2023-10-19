2a9f47686a1f446dafd508a4cfb195c2b671772c
Lane Bretschneider <lanebret85@gmail.com>
2023-10-19 16:44:40 -0700

    feat/refactor: re-organize controller to call on item_conditions private method to determine what to render, add conditions for edge cases in items find endpoint

--- app/controllers/api/v1/items/search_controller.rb
+++ app/controllers/api/v1/items/search_controller.rb
@@ -1,17 +1,32 @@
 class Api::V1::Items::SearchController < ApplicationController
   def index
-    if params[:name]
-      render json: ItemSerializer.new(Item.searched_by_name(Item, params[:name]).first)
-    elsif params[:min_price] && params[:max_price]
-      render json: ItemSerializer.new(Item.searched_by_both_prices(params[:min_price], params[:max_price]).first)
-    elsif params[:min_price]
-      render json: ItemSerializer.new(Item.searched_by_min_price(params[:min_price]).first)
-    elsif params[:max_price]
-      render json: ItemSerializer.new(Item.searched_by_max_price(params[:max_price]).first)
-    # elsif params[:name] && params[:min_price] || params[:name] && params[:max_price]
-
-    # else
-      
+    items = item_conditions
+    if items == "too_many_params_error"
+      render json: ErrorSerializer.new(ErrorMessage.new("Unable to process both name and price parameters", 400)).serialize_json, status: 400
+    elsif items == "invalid_price_error"
+      render json: ErrorSerializer.new(ErrorMessage.new("Prices may not be set below 0", 400)).serialize_json, status: 400
+    elsif !items.empty?
+      render json: ItemSerializer.new(items.first)
+    else
+      render json: {data: {}}
     end
   end
+
+  private
+
+    def item_conditions
+      if params[:name] && params[:min_price] || params[:name] && params[:max_price]
+        return "too_many_params_error"
+      elsif params[:min_price].to_f < 0 || params[:max_price].to_f < 0
+        return "invalid_price_error"
+      elsif params[:name]
+        return Item.searched_by_name(Item, params[:name])
+      elsif params[:min_price] && params[:max_price]
+        return Item.searched_by_both_prices(params[:min_price], params[:max_price])
+      elsif params[:min_price]
+        return Item.searched_by_min_price(params[:min_price])
+      elsif params[:max_price]
+        return Item.searched_by_max_price(params[:max_price])
+      end
+    end
 end
