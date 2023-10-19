c91e25bb387441624ab18660297cdfd5f269ff1a
Lane Bretschneider <lanebret85@gmail.com>
2023-10-18 20:37:38 -0700

    feat: add index action to render json depending on conditions being met

---
+++ app/controllers/api/v1/items/search_controller.rb
@@ -0,0 +1,17 @@
+class Api::V1::Items::SearchController < ApplicationController
+  def index
+    if params[:name]
+      render json: ItemSerializer.new(Item.searched_by_name(params[:name]).first)
+    elsif params[:min_price] && params[:max_price]
+      render json: ItemSerializer.new(Item.searched_by_both_prices(params[:min_price], params[:max_price]).first)
+    elsif params[:min_price]
+      render json: ItemSerializer.new(Item.searched_by_min_price(params[:min_price]).first)
+    elsif params[:max_price]
+      render json: ItemSerializer.new(Item.searched_by_max_price(params[:max_price]).first)
+    # elsif params[:name] && params[:min_price] || params[:name] && params[:max_price]
+
+    # else
+      
+    end
+  end
+end
